36e24d5ace67bf4800ab661610f63eb4a03d7bc5
b3m3 <b3m3@yandex.ru>
2022-03-03 11:39:10 +0200

    createt css html code

---
+++ js/app.js
@@ -0,0 +1,6 @@
+const wrapp = document.querySelector('.tic-tac-toe__wrapp');
+const boxes = document.querySelectorAll('.box');
+const soundStep = document.querySelector('.step');
+const soundWin = document.querySelector('.win');
+const soundDraw = document.querySelector('.draw');
+
